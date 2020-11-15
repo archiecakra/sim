74ef3a3a7d5fd89e36d2e2d5b168bd37dd90a3a5
titit <archiecakra1@gmail.com>
2020-11-15 18:41:30 +0700

    changing variable RouteServiceProvider@home

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -114,7 +114,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="{{ url('/data/stok') }}" class="nav-link">
+                    <a href="{{ url('/items') }}" class="nav-link">
                       <i class="far fa-circle nav-icon"></i>
                       <p>Data Stok Barang</p>
                     </a>
