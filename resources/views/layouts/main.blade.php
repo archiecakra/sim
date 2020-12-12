e9265fd1f0e96e70db8341151acf3b97c8145827
titit <archiecakra1@gmail.com>
2020-12-12 07:13:27 +0700

    UI Overhaul

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,10 +12,12 @@
   <link rel="stylesheet" href="{{ url('/css/OverlayScrollbars.min.css') }}">
     <!-- Theme style -->
   <link rel="stylesheet" href="{{ url('/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ url('/css/sim.css') }}">
     <!-- Google Font: Source Sans Pro -->
   <link href="{{ url('/css/googlefont.css') }}" rel="stylesheet">
+  {{-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> --}}
   </head>
-  <body class="hold-transition sidebar-mini layout-fixed text-sm">
+  <body class="hold-transition sidebar-mini layout-fixed">
   <!-- Site wrapper -->
     <div class="wrapper">
       <!-- Navbar -->
@@ -48,12 +50,9 @@
       <!-- Main Sidebar Container -->
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="../../index3.html" class="brand-link">
-          <img src="../../dist/img/AdminLTELogo.png"
-              alt="AdminLTE Logo"
-              class="brand-image img-circle elevation-3"
-              style="opacity: .8">
-          <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <a href="{{ url('/') }}" class="brand-link">
+          <img src="{{ url('gambar/site_assets/site_logo.png') }}" alt="Mentari Logo" class="brand-image">
+          <span class="brand-text font-weight-light">SIM Mentari</span>
         </a>
 
         <!-- Sidebar -->
@@ -61,7 +60,7 @@
           <!-- Sidebar user (optional) -->
           <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-              <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+              <img src="{{ url('gambar/site_assets/user_pic.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
               <a href="#" class="d-block">{{ Auth::user()->name }}</a>
@@ -71,8 +70,7 @@
           <!-- Sidebar Menu -->
           <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-              <!-- Add icons to the links using the .nav-icon class
-                  with font-awesome or any other icon font library -->
+              <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
               <li class="nav-item">
                 <a href="{{ url('/') }}" class="nav-link">
                   <i class="nav-icon fas fa-list-alt"></i>
@@ -81,6 +79,137 @@
                   </p>
                 </a>
               </li>
+              <li class="nav-header text-center nav-header-top"><h6 class="bg-secondary nav-header-title">Manajemen Stok</h6></li>
+              <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-box"></i>
+                  <p>
+                    Barang
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="{{ url('/transaction/sales') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Tambah Barang</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ url('/transaction/purchase') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Kelola Barang</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ url('/transaction/purchase') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Kelola Kategori</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-gifts"></i>
+                  <p>
+                    Pembelian Barang
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="{{ url('/transaction/sales') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Tambah Pembelian</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ url('/transaction/purchase') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Kelola Pembelian</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ url('/transaction/purchase') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Kelola Kategori</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-header text-center nav-header-top"><h6 class="bg-secondary nav-header-title">Penjualan</h6></li>
+              <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-shopping-cart"></i>
+                  <p>
+                    Transaksi Penjualan
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                  <a href="{{ url('/transaction/sales') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Tambah Transaksi</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ url('/transaction/purchase') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Kelola Transaksi</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-header text-center nav-header-top"><h6 class="bg-secondary nav-header-title">Manajemen Relasi</h6></li>
+              <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-th-list"></i>
+                  <p>
+                    Pelanggan
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="{{ url('/items') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Tambah Pelanggan</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ url('/suppliers') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Kelola Pelanggan</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fas fa-th-list"></i>
+                  <p>
+                    Supplier
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="{{ url('/items') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Tambah Supplier</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="{{ url('/suppliers') }}" class="nav-link">
+                      <i class="far fa-circle nav-icon"></i>
+                      <p>Kelola Supplier</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-header text-center"><h6 class="bg-secondary nav-header-title">Original Menu</h6></li>
               <li class="nav-item has-treeview">
                 <a href="#" class="nav-link">
                   <i class="nav-icon fas fa-shopping-cart"></i>
