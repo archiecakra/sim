3ac3c4436ab854a60490eefe3e235f7c0b17d74b
titit <titit.titit@titit.tit>
2021-01-08 07:34:45 +0700

    fixing login background

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="{{ url('/css/googlefont.css') }}" rel="stylesheet">
 </head>
-<body id="background" class="hold-transition login-page" style='background-image: url({{ url("/sites_img/login.jpg") }})'>
+<body id="background" class="hold-transition login-page" style='background-image: url({{ url("/img/sites_img/login.jpg") }})'>
 <div class="login-box">
   <div class="login-logo">
     <a href="#" class="text-white display-5"><b>Mentari</b> <br>Alat Tulis Grosir</a>
